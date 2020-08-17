0ab34002a6204b8a4acab1ceddcb9f3aca0faff2
coder-lzhh <616827451@qq.com>
2020-08-17 19:15:18 +0800

    init

---
+++ vue.config.js
@@ -0,0 +1,30 @@
+const path = require('path');//引入path模块
+const data = require('./data.json')
+
+function resolve(dir) {
+    return path.join(__dirname, dir)//path.join(__dirname)设置绝对路径
+}
+module.exports = {
+    chainWebpack: (config) => {
+        config.resolve.alias
+            .set('@', resolve('./src'))
+            .set('components', resolve('./src/components'))
+            .set('assets', resolve('./src/assets'))
+            .set('common', resolve('./src/components/common'))
+            .set('content', resolve('./src/components/content'))
+            .set('network', resolve('./src/network'))
+            .set('views', resolve('./src/views'))
+        //set第一个参数：设置的别名，第二个参数：设置的路径
+    },
+    devServer: {
+        before(app) {
+            app.get('/api/data', function (req, res) {
+                res.json({
+                    errno: 0,
+                    data: data
+                })
+            });
+        }
+    },
+
+}
